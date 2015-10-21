9b33e0afb8bfcb0836403996b047d03dc6bcba7c
Michael Sweeney <Michael@overthemike.local>
2015-10-21 11:50:05 -0700

    added templates

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
-  var names = [
-    'Domingo',
-    'Kristie',
-    'Phil',
-    'Noemi',
-    'Elijah',
-    'Ralph',
-    'Jaimarie',
-    'Thomas',
-    'Mychelle',
-    'Nick',
-    'Mike'
-  ];
+  var data = {
+    names: [
+      'Domingo',
+      'Kristie',
+      'Phil',
+      'Noemi',
+      'Elijah',
+      'Ralph',
+      'Jaimarie',
+      'Thomas',
+      'Mychelle',
+      'Nick',
+      'Mike'
+    ]
+  };
 
   var templateString = $("#listItemTemplate").text();
-  var renderTemplate = _.template(templateString);
-  var listHtml = renderTemplate(names);
-  $("#myList").html(listHtml);
+
+  var listHtml = Mustache.render(templateString, data);
+  console.log(listHtml);
+  $("#container").html(listHtml);
 });
