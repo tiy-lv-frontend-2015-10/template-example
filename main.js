d9996cd61f46bae2af45930444b8866f6ce6bca9
Michael Sweeney <Michael@overthemike.local>
2015-10-21 14:44:03 -0700

    added mustache example

--- main.js
+++ main.js
@@ -11,12 +11,24 @@ $(document).ready(function(){
       'Thomas',
       'Mychelle',
       'Nick',
+      'Monica',
       'Mike'
-    ]
+    ],
+    instructor: "Sweeney",
+    fruits: [
+      'Strawberries',
+      'Oranges',
+      'Apples'
+    ],
+    car: {
+      type: 'truck',
+      make: 'toyotoa',
+      model: 'tacoma'
+    }
   };
 
   var templateString = $("#listItemTemplate").text();
-
+  console.log(templateString);
   var listHtml = Mustache.render(templateString, data);
   console.log(listHtml);
   $("#container").html(listHtml);
